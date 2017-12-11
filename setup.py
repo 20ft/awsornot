93afb1ea15b074a3d741c9dc067c647b3d24bd39
David Preece <davep@zedkep.com>
2017-12-11 21:38:11 +1300

    Fixed contact details, version number

--- setup.py
+++ setup.py
@@ -15,10 +15,10 @@
 from setuptools import setup
 
 setup(name='awsornot',
-      version='0.7.1',
+      version='1.0.1',
       author='David Preece',
-      author_email='davep@polymath.tech',
-      url='https://polymath.tech',
+      author_email='davep@20ft.nz',
+      url='https://20ft.nz',
       license='BSD',
       packages=['awsornot'],
       install_requires=['requests', 'boto3'],
